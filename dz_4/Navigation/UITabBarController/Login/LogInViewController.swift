25b2c46ceaf2d6eaa52578730a6b02e82fb5b49a
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-02-21 12:54:03 +0300

    Добавлено изменение в проекте на тему "UIScrollView"

---
+++ dz_4/Navigation/UITabBarController/Login/LogInViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LogInViewController.swift
+//  Navigation
+//
+//  Created by Николай Гринько on 21.02.2023.
+//
+
+import Foundation
---
+++ dz_4/Navigation/UITabBarController/Login/LoginView.swift
@@ -0,0 +1,8 @@
+//
+//  LoginView.swift
+//  Navigation
+//
+//  Created by Николай Гринько on 21.02.2023.
+//
+
+import Foundation
