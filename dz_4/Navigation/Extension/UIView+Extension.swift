3892f22c60a7097a5ed6e780b88be4b7e4ad9d59
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-02-27 00:03:48 +0300

    Added modified work_"UITableView, UIScrollView, datasource и delegate" часть 2"

---
+++ dz_4/Navigation/Extension/UIView+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Extension.swift
+//  Navigation
+//
+//  Created by Николай Гринько on 26.02.2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
